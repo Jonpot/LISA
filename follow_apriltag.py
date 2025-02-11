798f8c1dee5f74c23c4d2376a3159167e56fc94c
Mercury <38762173+Europix@users.noreply.github.com>
2025-02-11 13:54:54 -0500

    Updated "swinging" mode
    actively searching for apriltag between positions if not found one.

--- follow_apriltag.py
+++ follow_apriltag.py
@@ -22,17 +22,25 @@ robot_connection.create_connection()
 camera = AprilTagDetector(robot_connection)
 mover = ArmMover(robot_connection)
 
-home = [0.57, 0.00, 0.42, 90, 0, 90]
+home = [0.57, 0.00, 0.42, 90, 0, 90] # position 3
+sentry_position1 = [0.42, -0.40, 0.42, 90, 0, 46]
+sentry_position2 = [0.53, -0.23, 0.42, 90, 0, 66]
+sentry_position3 = home
+sentry_position4 = [0.54, 0.20, 0.42, 90, 0, 110]
+positions_list = [sentry_position1, sentry_position2, sentry_position3, sentry_position4]
+
 home_status = mover.arbitrary_movement(home[0], home[1], home[2], home[3], home[4], home[5])
 if not home_status:
     print("Failed to move to home position")
     robot_connection.close_connection()
     exit()
-
 import time
 import math
+frame_count = 0
+current_position_in_list = 2 # home position
 while True:
     # Detect the apriltag
+    frame_count = 0
     detection_information = camera.detect_apriltag()
     if detection_information[0] is not None:
         y = detection_information[0] * -1
@@ -60,9 +68,20 @@ while True:
         cv2.circle(image, (int(y), int(z)), 10, (0, 255, 0))
         cv2.imshow("AprilTag Detection", image)
     else:
+        frame_count += 1
         _, image = camera.video_capture.read()
         ## Show the image with a big red border to indicate no apriltag
         image = cv2.copyMakeBorder(image, 100, 100, 100, 100, cv2.BORDER_CONSTANT, value=(0, 0, 255))
+
+        # move in different positions if 10 frames have passed
+        if frame_count >= 60:
+            next_position = (current_position_in_list+1) % 4
+            position = positions_list[next_position]
+            print(f"Moving to position {next_position}")
+            mover.arbitrary_movement(position[0], position[1], position[2], position[3], position[4], position[5])
+            current_position_in_list = next_position
+        frame_count = 0
+
         cv2.imshow("AprilTag Detection", image)
     
     
